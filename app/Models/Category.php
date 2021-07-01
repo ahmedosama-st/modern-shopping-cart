c8ff3f4f0c7140c9e4a29cd814713177014432e6
Ahmed Osama <marvelphp5@gmail.com>
2021-07-01 19:20:23 +0200

    Added categories feature with tests

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\{HasChildren, Orderable};
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Category extends Model
+{
+    use HasFactory, HasChildren, Orderable;
+
+    protected $fillable = [
+        'name',
+        'order'
+    ];
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id', 'id');
+    }
+}
---
+++ tests/Unit/Models/Categories/CategoryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit\Models\Categories;
+
+use Tests\TestCase;
+use App\Models\Category;
+
+class CategoryTest extends TestCase
+{
+    public function test_it_has_many_children()
+    {
+        $category = Category::factory()->create();
+
+        $category->children()->save(
+            Category::factory()->create()
+        );
+
+        $this->assertInstanceOf(Category::class, $category->children->first());
+    }
+
+    public function test_it_is_orderable()
+    {
+        $category = Category::factory()->create([
+            'order' => 2
+        ]);
+
+        $anotherCategory = $category->children()->save(
+            Category::factory()->create([
+                'order' => 1
+            ])
+        );
+
+        $this->assertEquals($anotherCategory->name, Category::ordered()->first()->name);
+    }
+
+    public function test_it_can_fetch_only_parents()
+    {
+        $category = Category::factory()->create();
+
+        $category->children()->save(
+            Category::factory()->create()
+        );
+
+        $this->assertEquals(1, Category::parents()->count());
+    }
+}
