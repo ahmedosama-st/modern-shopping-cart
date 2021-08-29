23291d37e18c26c6b59f81318452c8d0126a2b52
Ahmed Osama <marvelphp5@gmail.com>
2021-08-29 16:29:11 +0200

    Added countries table and seeder

---
+++ app/Models/Country.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Country extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'code', 'name'
+    ];
+}
